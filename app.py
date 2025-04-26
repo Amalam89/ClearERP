6234bf8c893747049d3e00580586f49f61d2e8d1
Ali Malam <malama@uci.edu>
2025-04-25 20:00:06 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/contact', methods=['POST'])
+def contact():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+
+    # Send to Formspree (replace the URL with your own below)
+    requests.post('https://formspree.io/f/mpwdzrqg', data={
+        'name': name,
+        'email': email,
+        'message': message
+    })
+
+    return redirect('/')  # Send them back to homepage (or a thank-you page)
+
+@app.route('/privacy', methods=['GET'])
+def privacy():
+    return render_template('privacy.html')
+
+@app.route('/terms', methods=['GET'])
+def terms():
+    return render_template('terms.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
